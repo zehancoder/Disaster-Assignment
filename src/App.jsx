1db568e77e41062556691b616dd99b3773bf13b9
zehancoder <zehanjh93@gmail.com>
2025-02-02 20:38:26 +0600

    Setup complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-center text-gray-600 '>Hello World</h1>
+      <Button variant='contained'>
+        Click
+      </Button>
+
+    </>
+  )
+}
+
+export default App
